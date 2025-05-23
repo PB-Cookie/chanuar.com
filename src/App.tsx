8717feb7aa0debb087a940d519d1609955590f15
PB-Cookie <rexwin9@gmail.com>
2025-05-23 23:57:59 +0100

    add framer motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
+import { motion } from "framer-motion";
+
 function App() {
   return (
-    <>
+    <motion.div
+      initial={{ y: 150, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ duration: 1.5 }}
+    >
       <h1>🚧Site is under construction🚧</h1>
       <h2>
         You can check my <a href="https://github.com/PB-Cookie">github</a> on
         the meanwhile
       </h2>
-    </>
+    </motion.div>
   );
 }
 
